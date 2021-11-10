ec24b30bab57f5a1f170a7c85b1f3b0e46baa6dc
DurugyAdam <adam.durugy@gmail.com>
2021-11-10 11:24:35 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -53,3 +53,16 @@ $response = tap($kernel->handle(
 ))->send();
 
 $kernel->terminate($request, $response);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Első</title>
+</head>
+<body>
+    <h1>Főoldal</h1>
+</body>
+</html>
